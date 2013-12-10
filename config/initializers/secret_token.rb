4769ae8bd41cc6a5e47f44cfc73ef848b5c031ff
Alexander Larson <alexanderlarson@gmail.com>
2013-12-10 16:03:27 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hopular::Application.config.secret_key_base = '4b5258bfd7bcdf6fb1e15b85a92e0c2d8209cf01979a797d4109c591e1266de8e9011ce3219db14e6df3bd604464627b3c04949e917280849f844271a0a7e2c4'
